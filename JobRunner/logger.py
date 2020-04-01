bcb45c896d2a3cec491fdc67309141e0597d1547
Boris Sadkhin <boris@bortech.us>
2020-04-01 00:29:46 -0500

    Fixed boolean

--- JobRunner/logger.py
+++ JobRunner/logger.py
@@ -30,7 +30,7 @@ class Logger(object):
     def log(self, line, ts=None):
         if self.debug:  # pragma: no cover
             print(line, flush=True)
-        line = {'line': line, 'is_error': 0}
+        line = {'line': line, 'is_error': False}
         if ts:
             line['ts'] = ts
 
@@ -39,7 +39,7 @@ class Logger(object):
     def error(self, line, ts=None):
         if self.debug:  # pragma: no cover
             print(line, flush=True)
-        line = {'line': line, 'is_error': 1}
+        line = {'line': line, 'is_error': True}
         if ts:
             line['ts'] = ts
         self.ee2.add_job_logs({'job_id': self.job_id}, [line])
