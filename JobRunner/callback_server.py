55e284c14610bb9af764b14654b16d56d24996fe
Boris Sadkhin <boris@bortech.us>
2020-05-11 14:29:21 -0500

    Adding debug statements

--- JobRunner/callback_server.py
+++ JobRunner/callback_server.py
@@ -20,13 +20,16 @@ def start_callback_server(ip, port, out_queue, in_queue, token, bypass_token):
         "bypass_token": bypass_token,
     }
     app.config.update(conf)
-    app.run(host=ip, port=port, debug=False, access_log=False)
+    app.run(host=ip, port=port, debug=False, access_log=True)
+
 
 
 @app.route("/", methods=["GET", "POST"])
 async def root(request):
+    logger.info("Hit the root")
     try:
         data = request.json
+        logger.warning(f"{data}")
         if request.method == "POST" and data is not None and "method" in data:
             token = request.headers.get("Authorization")
             return json(await _process_rpc(data, token))
@@ -39,22 +42,26 @@ async def root(request):
 
 @add_status_code(500)
 def _job_failed(output):
+    logger.info(f"JOB FAILED!")
     if 'error' in output:
         output['result'] = output['error']
     return output
 
 
-def _check_finished():
+def _check_finished(info=None):
     global prov
     in_q = app.config["in_q"]
     try:
         # Flush the queue
         while True:
+            logger.info(f"Hit the _check_finished for {info}")
             [mtype, fjob_id, output] = in_q.get(block=False)
+            logger.info(f"Got in_q check finished of {[mtype, fjob_id, output]}")
+
             if mtype == "output":
                 if 'error' in output:
                     _job_failed(output)
-
+                logger.info(f"Job output is OK")
                 outputs[fjob_id] = output
             elif mtype == "prov":
                 prov = output
@@ -63,11 +70,13 @@ def _check_finished():
 
 
 def _check_rpc_token(token):
+    logger.info("Hit the rpc token check")
     if token != app.config.get("token"):
         if app.config.get("bypass_token"):
             pass
         else:
             abort(401)
+    logger.info("success")
 
 
 def _handle_provenance():
@@ -103,15 +112,19 @@ async def _process_rpc(data, token):
     (module, method) = data["method"].split(".")
     # async submit job
     if method.startswith("_") and method.endswith("_submit"):
+        logger.info("Hit the async")
         return _handle_submit(module, method, data, token)
     # check job
     elif method.startswith("_check_job"):
+        logger.info("Hit the checkjob")
         return _handle_checkjob(data=data)
     # Provenance
     elif method.startswith("get_provenance"):
+        logger.info("Hit the prov")
         return _handle_provenance()
     else:
         # Sync Job
+        logger.info("Hit the sync method for " + data["method"])
         _check_rpc_token(token)
         job_id = str(uuid.uuid1())
 
@@ -119,12 +132,15 @@ async def _process_rpc(data, token):
         logger.info(data["method"])
         data["method"] = "%s.%s" % (module, method)
 
-
+        logger.info("Hit the sync 2 for" + data["method"])
         app.config["out_q"].put(["submit", job_id, data])
+        logger.info("Hit the sync 3 for" + data["method"])
         try:
             while True:
+                logger.info("Hit the sync loop for" + data["method"])
                 _check_finished()
                 if job_id in outputs:
+                    logger.info("Hit the sync output stage")
                     resp = outputs[job_id]
                     resp["finished"] = 1
                     return resp
