325c19edb1c27143a5e6045c14d4f48eb4c1c24c
bio-boris <bioboris@github.com>
2020-06-18 18:17:53 -0500

    Close then terminate

--- JobRunner/JobRunner.py
+++ JobRunner/JobRunner.py
@@ -377,7 +377,8 @@ class JobRunner(object):
             config=config, job_id=self.job_id, job_params=job_params, subjob=False
         )
         output = self._watch(config)
-        self.cbs.kill()
+        self.cbs.close()
+        self.cbs.terminate()
         self.logger.log("Job is done")
 
         error = output.get("error")
